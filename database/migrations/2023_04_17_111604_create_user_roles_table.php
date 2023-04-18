af90539ae169a446622a55ac3af86bcce9ca413f
a_z <azdvd@mail.ru>
2023-04-18 19:47:07 +0300

    seeders for roles and permissions part 3

--- database/migrations/2023_04_17_111604_create_user_roles_table.php
+++ database/migrations/2023_04_17_111604_create_user_roles_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -13,6 +12,7 @@ return new class extends Migration
      */
     public function up(): void
     {
+        // не нужен после установки spatie/laravel-permission
         Schema::create('user_roles', function (Blueprint $table) {
             $table->bigInteger('user_id')->unsigned()->index();
             $table->bigInteger('role_id')->unsigned()->index();
