991fd3d55e019c469017b037d9bf5a79b4d60630
mikgir <groshevm@mail.ru>
2023-04-18 11:53:40 +0300

    add Contracts/Repository/UserRepositoryInterface, Repositories/UserRepository

---
+++ app/Repositories/UserRepository.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Contracts\Repositories\UserRepositoryInterface;
+
+class UserRepository implements UserRepositoryInterface
+{
+
+    public function all(): array
+    {
+        // TODO: Implement all() method.
+    }
+
+    public function oneById(int $id): object
+    {
+        // TODO: Implement oneById() method.
+    }
+}
