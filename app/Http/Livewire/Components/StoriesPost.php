4a889a58709a216c4e2105731bc1cd6de0901a73
mikgir <groshevm@mail.ru>
2023-04-20 16:12:32 +0300

    main page is ready to add logic

---
+++ app/Http/Livewire/Components/StoriesPost.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Components;
+
+use Livewire\Component;
+
+class StoriesPost extends Component
+{
+    public function render()
+    {
+        return view('livewire.components.stories-post');
+    }
+}
